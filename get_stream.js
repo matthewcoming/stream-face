b03bc3da9bd05b29c93a437979525b9e0b156a4e
Matthew Coming <coming.matthew@gmail.com>
2021-04-21 15:30:20 -0500

    changed get_stream.js to use new twitch-m3u8 API

--- get_stream.js
+++ get_stream.js
@@ -1,6 +1,6 @@
 // replace clientID with your Twitch Client ID
-const twitch = require('twitch-m3u8')('kimne78kx3ncx6brgo4mv6wki5h1ko');
+const twitch = require('twitch-m3u8');
 
-twitch.getURL(process.argv[2], false)
-  .then(data => console.log(data))
+twitch.getStream(process.argv[2], false)
+  .then(data => console.log(data[0].url))
   .catch(err => console.error(err))
